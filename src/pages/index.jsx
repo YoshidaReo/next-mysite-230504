db5f5ecafddcdf6d9d04cf835e2c9e62f4386eed
YoshidaReo <happypan86@gmail.com>
2023-05-05 21:36:09 +0900

    ✨ useStateで文字列と真偽値について解説。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+
+
   // let foo = 1;
 
   // メソットを書くのはコンポーネント内外に書く。内部で引数を再レンダリングしないようするためにuseCallbackを使う。
@@ -32,6 +36,18 @@ export default function Home() {
     };
   }, [count]);
 
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length >= 5) {
+      alert("5文字以内にしてください")
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
   return (
     <>
       <Head>
@@ -45,9 +61,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Header />
-        <h1>
-          {count}
-        </h1>
+        {isShow ? <h1>{count}</h1> : null}
 
         <button
           href='/about'
@@ -55,6 +69,12 @@ export default function Home() {
         >
           ボタン
         </button>
+
+        <button onClick={handleDisplay}>
+          {isShow ? "非表示" : "表示"}
+        </button>
+
+        <input type="text" value={text} onChange={handleChange}/>
 
         <Headline title="Main">
           <code className={styles.code}>pages/Main.js</code>
